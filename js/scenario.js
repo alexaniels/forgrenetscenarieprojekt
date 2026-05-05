bdc8fb155f6f75bdf1fbeae8d034389eca6800d8
Alexandra Nielsen <alexandrat.nielsen@gmail.com>
2026-05-06 01:49:33 +0200

    Testing js codes, to create the interactions with btn. Changing class names in html and scss.

--- js/scenario.js
+++ js/scenario.js
@@ -1,3 +1,1 @@
-const btns = document.querySelectorAll(".btn");
-const scenarios = document.querySelectorAll(".scenario");
-const main = document.querySelectorAll("main")
+//console.log("Halløjsa fra scenariet"); test //
